b85e9276865c508be410076a2523e7f83a9754ba
John Reese <bennoosterholt@gmail.com>
2021-09-02 22:35:15 +0200

    added the pretty print function

--- setup-profile-on-new-box.bash
+++ setup-profile-on-new-box.bash
@@ -39,6 +39,90 @@ VERSION:
 EOF
 }
 
+
+print() {
+
+	# DESC: pretty print text, lines
+	# ARGS: 1 -> Color, line
+	#		choices are:
+	#			RED
+	#			YELLOW
+	#			BLUE
+	#			GREEN
+	#			NOC (no color)
+	#			LINE
+	#
+	# ARGS: 2 -> Text, line element
+	# OUTS: colorised text, (non-colorised) line
+	# LIMITS:
+	#		cannot take formatters (newline,tabs,etc...)
+	# EXAMPLE:
+	#		print RED "text to be printed red"
+	#		print "text to be printed without color"
+	#		print LINE
+	#		print LINE #
+
+	case "${1^^}" in
+	RED)
+		printf '\033[0;31m%s\033[0m\n' "${2}"
+		;;
+	YELLOW)
+		printf '\e[33m%s\033[0m\n' "${2}"
+		;;
+	BLUE)
+		printf '\e[33m%s\033[0m\n' "${2}"
+		;;
+	GREEN)
+		printf '\e[0;32m%s\033[0m\n' "${2}"
+		;;
+	LOG)
+		printf '%(%F %T)T :: %s\n' -1 "${2}"
+		;;
+
+	# this option is for running a script with systemd
+	# systemd does not create a terminal (and its size),
+	# so the $TERM variable stays empty. using a fixed length
+	LOGLINE)
+		separator="-" # separator default
+		logline="["
+		for ((i = 1; i <= 50; i++)); do # make the line
+			logline+="${separator}"
+		done
+		logline+="]"
+		printf '%s\n' "${logline}" # regurgitate to terminal
+		;;
+	TIMELINE)
+		separator="-"            # separator default
+		line="[---[$(date +"%Y-%m-%d %H:%M:%S")]"                 # adding to a "line" variable
+		term_size="$(tput cols)" # get number of columns
+		if ([[ ! -z "$2" ]] && [[ "${#2}" == 1 ]]); then # set custom the separator (length must be 1)
+			separator="${2}"
+		fi
+		for ((i = 1; i <= "${term_size}-26"; i++)); do # make the line
+			line+="${separator}"
+		done
+		line+="]"
+		printf '%s\n' "${line}" # regurgitate to terminal
+		;;	
+	LINE)
+		separator="-"            # separator default
+		line="["                 # adding to a "line" variable
+		term_size="$(tput cols)" # get number of columns
+		if ([[ ! -z "$2" ]] && [[ "${#2}" == 1 ]]); then # set custom the separator (length must be 1)
+			separator="${2}"
+		fi
+		for ((i = 1; i <= "${term_size}-2"; i++)); do # make the line
+			line+="${separator}"
+		done
+		line+="]"
+		printf '%s\n' "${line}" # regurgitate to terminal
+		;;
+	*)
+		printf '\033[0m%s\033[0m\n' "${1}"
+		;;
+	esac
+}
+
 function script_finish {
 
 	# DESC: Trap exits with cleanup function
@@ -78,7 +162,7 @@ function main {
 
 	# vars
 	readonly _directory=".bashrc.d"
-	readonly url="https://raw.githubusercontent.com/rogrwhitakr/northern-lights/master/conf/dotfiles/.bashrc.d"
+	readonly url="https://raw.githubusercontent.com/rogrwhitakr/dotfiles/main/.bashrc.d"
 
 	declare -a sources=('.bashrc' '.bash_profile')
 	declare -a files=('alias.bash' 'function.bash' 'export.bash' 'program.bash')
@@ -89,6 +173,8 @@ function main {
 	# we create in the home of the user excuting the unit file!
 	cd "$HOME"
 
+	print line
+	print RED "Starting"
 	# we create .bashrc if it doesn't exist
 	for source in "${sources[@]}"; do
 		if [[ ! -f "$HOME"/"${source}" ]]; then
@@ -129,6 +215,8 @@ function main {
 
 	printf "\nCompleted."
 	printf "\n"
+	print line
+	print GREEN "Finished"
 }
 
 # Make it rain
